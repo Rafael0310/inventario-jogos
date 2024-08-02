878f888e0e5eb67e79298461cd2a56c03b77ac94
Rafael Abreu <rafael.abreu031015@gmail.com>
2024-08-01 23:28:50 -0300

    finish project

--- inventario.py
+++ inventario.py
@@ -4,7 +4,6 @@ from metodos import ( continuar, limpar_tela )
 
 # Importando bibliotecas
 from dataclasses import dataclass
-import os
 
 jogos = []
 
@@ -13,55 +12,77 @@ class Inventario:
     
     @classmethod
     def adicionar_jogo(cls):
-        limpar_tela()
-        titulo = input('Insira o nome do jogo que deseja adicionar: ').lower().strip()
+        while True:
+            limpar_tela()
+            titulo = input('Insira o nome do jogo que deseja adicionar: ').upper().strip()
 
-        if not cls.procurar_jogo(titulo):
-            try:
-                genero = input('Insira o gênero do jogo: ')
-                release = int(input('Insira o ano de lançamento do jogo:'))
-                jogos.append(Jogo(titulo=titulo, genero=genero, release=release))
+            if not cls.procurar_jogo(titulo):
+                try:
+                    genero = input('Insira o gênero do jogo: ').upper().strip()
+                    release = int(input('Insira o ano de lançamento do jogo: '))
+                    jogos.append(Jogo(titulo=titulo, genero=genero, release=release))
 
-                print(f'\nNovo jogo adicionado!\nNome: {jogos[-1].titulo}\nGênero: {jogos[-1].genero}\nLançamento: {jogos[-1].release}\n')
+                    print(f'\nNovo jogo adicionado!\nNome: {jogos[-1].titulo}\nGênero: {jogos[-1].genero}\nLançamento: {jogos[-1].release}\n')
 
-            except ValueError:
-                print('Valor inválido.\n')
+                except ValueError:
+                    print('Valor inválido.\n')
 
-        else:
-            print('Esse jogo já foi incluido no inventário.')
+            else:
+                print('Esse jogo já foi incluido no inventário.')
+
+            if not continuar():
+                break
             
 
     @classmethod
     def remover_jogo(cls):
-        limpar_tela()
-        if cls.verificar_lista():
-            titulo = input('Insira o nome do jogo que deseja excluir: ').lower().strip()
-            if not cls.procurar_jogo(titulo):
-                pass
+        while True:
+            limpar_tela()
+
+            if cls.verificar_lista():
+                titulo = input('Insira o nome do jogo que deseja excluir: ').upper().strip()
+
+                if cls.procurar_jogo(titulo):
+                    print('Jogo não localizado.')
+
+                else:
+                    for item in jogos:
+                        if item.titulo == titulo:
+                            jogos.remove(item)
+                            print('Jogo removido.')
+                            break
 
     @classmethod
     def listar_jogos(cls):
         limpar_tela()
         if cls.verificar_lista():
-            pass
+            for item in jogos:
+                print(f'Jogo: {item.titulo}\nGênero: {item.genero}\nLançamento: {item.release}\n\n')
 
     @classmethod
     def listar_por_titulo(cls):
         limpar_tela()
         if cls.verificar_lista():
-            pass
+            jogos_ordem_titulo = sorted(jogos, key=lambda x: x.titulo)
+            for item in jogos_ordem_titulo:
+                print(f'Jogo: {item.titulo}\nGênero: {item.genero}\nLançamento: {item.release}\n\n')
 
     @classmethod
     def listar_por_genero(cls):
         limpar_tela()
         if cls.verificar_lista():
-            pass
+            jogos_ordem_genero = sorted(jogos, key=lambda x: x.genero)
+            for item in jogos_ordem_genero:
+                print(f'Jogo: {item.titulo}\nGênero: {item.genero}\nLançamento: {item.release}\n\n')
 
     @classmethod
     def listar_por_release(cls):
         limpar_tela()
+
         if cls.verificar_lista():
-            pass
+            jogos_ordem_release = sorted(jogos, key=lambda x: x.release)
+            for item in jogos_ordem_release:
+                print(f'Jogo: {item.titulo}\nGênero: {item.genero}\nLançamento: {item.release}\n\n')
 
     
     @staticmethod
